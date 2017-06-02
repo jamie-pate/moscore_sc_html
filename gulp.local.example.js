c5e838f3a5fa971b490c48bde1e767fd479b618a
jamie.pate@gmail.com <jamie.pate@gmail.com>
2017-06-01 22:27:42 -0700

    Move gulp.local.js to gulp.local.example.js

---
+++ gulp.local.example.js
@@ -0,0 +1,5 @@
+// rename this file as gulp.local.js
+var m_gulp = require('../moscore_ng/mosco_gulp.js');
+m_gulp.config({
+	build_dest: '../moscore_qm/run/scoreboard'
+});
--- gulp.local.js
+++
@@ -1,4 +0,0 @@
-var m_gulp = require('../moscore_ng/mosco_gulp.js');
-m_gulp.config({
-	build_dest: '../moscore_qm_branch/run/scoreboard'
-});
